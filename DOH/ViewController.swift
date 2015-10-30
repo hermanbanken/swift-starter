64867e83068b10cb6736a83eb4b96ec73f567a96
Herman Banken <hermanbanken@gmail.com>
2015-10-30 12:48:45 +0100

    Initial Commit

---
+++ DOH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DOH
+//
+//  Created by H.J. Banken on 30/10/15.
+//  Copyright © 2015 nl.tudelft.ch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ DOHTests/DOHTests.swift
@@ -0,0 +1,36 @@
+//
+//  DOHTests.swift
+//  DOHTests
+//
+//  Created by H.J. Banken on 30/10/15.
+//  Copyright © 2015 nl.tudelft.ch. All rights reserved.
+//
+
+import XCTest
+@testable import DOH
+
+class DOHTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
